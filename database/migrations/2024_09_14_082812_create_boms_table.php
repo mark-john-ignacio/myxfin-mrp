70ac4110614bfcb0b19ee1c1f4c000fd18f326ee
Mark John Ignacio <markjohn.ignacio@cvsu.edu.ph>
2024-09-16 16:50:31 +0800

    uncommented

--- database/migrations/2024_09_14_082812_create_boms_table.php
+++ database/migrations/2024_09_14_082812_create_boms_table.php
@@ -1,29 +1,29 @@
 <?php
-//
-//use Illuminate\Database\Migrations\Migration;
-//use Illuminate\Database\Schema\Blueprint;
-//use Illuminate\Support\Facades\Schema;
-//
-//return new class extends Migration
-//{
-//    /**
-//     * Run the migrations.
-//     */
-//    public function up()
-//    {
-//        Schema::create('boms', function (Blueprint $table) {
-//            $table->id();
-//            $table->string('product_name');
-//            $table->enum('bom_type', ['Standard/Manufacture', 'WIP']);
-//            $table->integer('quantity');
-//            $table->string('uom');
-//            $table->foreignId('inventory_id')->constrained('inventory')->onDelete('cascade');
-//            $table->timestamps();
-//        });
-//    }
-//
-//    public function down()
-//    {
-//        Schema::dropIfExists('boms');
-//    }
-//};
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('boms', function (Blueprint $table) {
+            $table->id();
+            $table->string('product_name');
+            $table->enum('bom_type', ['Standard/Manufacture', 'WIP']);
+            $table->integer('quantity');
+            $table->string('uom');
+            $table->foreignId('inventory_id')->constrained('inventory')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('boms');
+    }
+};
--- database/migrations/2024_09_14_082815_create_job_order_items_table.php
+++ database/migrations/2024_09_14_082815_create_job_order_items_table.php
@@ -1,27 +1,27 @@
 <?php
-//
-//use Illuminate\Database\Migrations\Migration;
-//use Illuminate\Database\Schema\Blueprint;
-//use Illuminate\Support\Facades\Schema;
-//
-//return new class extends Migration
-//{
-//    /**
-//     * Run the migrations.
-//     */
-//    public function up()
-//    {
-//        Schema::create('job_order_items', function (Blueprint $table) {
-//            $table->id();
-//            $table->foreignId('job_order_id')->constrained('job_order')->onDelete('cascade');
-//            $table->string('product_name');
-//            $table->integer('quantity');
-//            $table->timestamps();
-//        });
-//    }
-//
-//    public function down()
-//    {
-//        Schema::dropIfExists('job_order_items');
-//    }
-//};
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up()
+    {
+        Schema::create('job_order_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('job_order_id')->constrained('job_order')->onDelete('cascade');
+            $table->string('product_name');
+            $table->integer('quantity');
+            $table->timestamps();
+        });
+    }
+
+    public function down()
+    {
+        Schema::dropIfExists('job_order_items');
+    }
+};
